d6447b0025be1f1fb444e2e743d61e121371d941
cgtykarasu <cgtykarasu@CGTY>
2021-02-22 18:08:31 +0300

    day3hw3

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer (Customer customer)
+        {
+            Console.WriteLine("Müşteri Id : "+ customer.Id +
+                "\nMüşteri Adı : " + customer.CustomerName +
+                "\nMüşteri Soyadı : " + customer.CustomerSurname +
+                "\nMüşteri TCKN : " + customer.TCKN +
+                "\nMüşteri Telefon No : " + customer.CustomerPhoneNumber + 
+                "\n########################################\n");
+        }
+
+        public void ListCustomer(Customer[] customerList)
+        {
+            foreach (var customer in customerList)
+            {
+                if (customer != null)
+                {
+                    Console.WriteLine("Müşteri Id : " + customer.Id +
+                        "\nMüşteri TCKN : " + customer.TCKN +
+                        "\nMüşteri Adı ve Soyadı : " + customer.CustomerName + " " + customer.CustomerSurname +
+                        "\nMüşteri Telefon No : " + customer.CustomerPhoneNumber);
+                }
+                else
+                    continue;
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int customerId = 000;
+
+            Customer customer01 = new Customer();
+            customerId++;
+            customer01.Id = customerId;
+            customer01.CustomerName = "Çağatay";
+            customer01.CustomerSurname = "KARASU";
+            customer01.TCKN = "0123456789";
+            customer01.CustomerPhoneNumber = "+905554443322";
+
+            Customer customer02 = new Customer();
+            customerId++;
+            customer02.Id = customerId;
+            customer02.CustomerName = "Gökhan";
+            customer02.CustomerSurname = "TÜRKMEN";
+            customer02.TCKN = "9876543210";
+            customer02.CustomerPhoneNumber = "+905443332211";
+
+            Customer[] customerList = new Customer[] { customer01, customer02 };
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.AddCustomer(customer01);
+            customerManager.AddCustomer(customer02);
+
+            customerManager.ListCustomer(customerList);
+
+        }
+    }
+}
